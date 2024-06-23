69b3e5562e411aac00471d5b85154a7ad7278871
chitra-droid <ucb9460@gmail.com>
2024-06-23 12:24:36 +0530

    adding solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+  RWOnly ch = new RWOnly();
+  ch.setName("chitra");
+  System.out.print(ch.getName());
+
+}
 }
